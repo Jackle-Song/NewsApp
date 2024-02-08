7b18774db31cfb67e545618c60f0938b80b1366b
Jackle Song <jacklesong921@gmail.com>
2024-02-08 14:45:04 +0800

    init project

---
+++ app/src/main/java/com/mrsworkshop/newsapp/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mrsworkshop.newsapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrsworkshop.newsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
